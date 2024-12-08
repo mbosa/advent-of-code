174604f364241451dc525d875f1df3b8025b9ce8
mbosa <marco.bosa991@gmail.com>
2024-12-08 19:49:27 +0100

    2024 day 03

---
+++ 2024/03/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	_ "embed"
+
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+//go:embed input.txt
+var input string
+
+var mulRegexPart1 = regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+var mulRegexPart2 = regexp.MustCompile(`do\(\)|don't\(\)|mul\((\d+),(\d+)\)`)
+
+func main() {
+	res1, res2 := solve(input)
+
+	fmt.Println("part1:", res1)
+	fmt.Println("part2:", res2)
+}
+
+func solve(input string) (int, int) {
+	res1 := part1(input)
+	res2 := part2(input)
+
+	return res1, res2
+}
+
+func part1(input string) int {
+	res := 0
+
+	matches := mulRegexPart1.FindAllStringSubmatch(input, -1)
+
+	for _, match := range matches {
+		n1, _ := strconv.Atoi(match[1])
+		n2, _ := strconv.Atoi(match[2])
+		res += (n1 * n2)
+	}
+
+	return res
+}
+
+func part2(input string) int {
+	res := 0
+
+	matches := mulRegexPart2.FindAllStringSubmatch(input, -1)
+
+	enabled := true
+
+	for _, match := range matches {
+		if match[0] == "do()" {
+			enabled = true
+		} else if match[0] == "don't()" {
+			enabled = false
+		} else {
+			if enabled {
+				n1, _ := strconv.Atoi(match[1])
+				n2, _ := strconv.Atoi(match[2])
+				res += (n1 * n2)
+
+			}
+		}
+	}
+
+	return res
+}
---
+++ 2024/03/main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+)
+
+const testInput = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5)_mul(1,2))"
+
+func TestSolveTestInput(t *testing.T) {
+	want1 := 163
+	want2 := 50
+
+	got1, got2 := solve(testInput)
+
+	if got1 != want1 {
+		t.Errorf("part1: want %d, got %d", want1, got1)
+	}
+	if got2 != want2 {
+		t.Errorf("part2: want %d, got %d", want2, got2)
+	}
+}
+
+func TestSolveInput(t *testing.T) {
+	want1 := 157621318
+	want2 := 79845780
+
+	got1, got2 := solve(input)
+
+	if got1 != want1 {
+		t.Errorf("part1: want %d, got %d", want1, got1)
+	}
+	if got2 != want2 {
+		t.Errorf("part2: want %d, got %d", want2, got2)
+	}
+}
+
+func BenchmarkSolve(b *testing.B) {
+	for range b.N {
+		solve(input)
+	}
+}
